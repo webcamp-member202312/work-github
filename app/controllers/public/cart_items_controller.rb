e5614a535202b24f7e6cee131d859a5202a08388
EC2 Default User <149689018+furumasa127@users.noreply.github.com>
2024-01-09 13:21:48 +0000

    [Finish] カート機能まで

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,31 +1,45 @@
 class Public::CartItemsController < ApplicationController
   before_action :authenticate_customer!
-  
+
   def index
-    @cart_item = CartItem.new
-    @cart_items = CartItem.all
+    @cart_items = current_customer.cart_items
   end
-  
+
   def update
+    cart_item = current_customer.cart_items.find(params[:id])
+    cart_item.update(cart_item_params)
+    redirect_back(fallback_location: root_path)
   end
-  
+
   def destroy
-  end
-  
-  def destroy_all
-  end
-  
-  def create
-    @cart_item = CartItem.new(cart_item_params)
-    @cart_item.item_id = current_customer.id
-    @cart_item.save
+    cart_item = current_customer.cart_items.find(params[:id])
+    cart_item.destroy
     redirect_to cart_items_path
-    
   end
-  
+
+  def destroy_all
+    current_customer.cart_items.destroy_all
+    redirect_to cart_items_path
+  end
+
+  def create
+      cart_item = CartItem.new(cart_item_params)
+      cart_item.customer_id = current_customer.id
+      cart_item.item_id = cart_item_params[:item_id]
+    if current_customer.cart_items.find_by(item_id: params[:cart_item][:item_id]).present?
+      cart_item = current_customer.cart_items.find_by(item_id: params[:cart_item][:item_id])
+      cart_item.amount += params[:cart_item][:amount].to_i
+      cart_item.update(amount: cart_item.amount)
+      redirect_to cart_items_path
+    else
+      cart_item.save
+      redirect_to cart_items_path
+    end
+  end
+
   private
- 
+
   def cart_item_params
-    params.require(:cart_item).permit(:item_id, :amount)
+    params.require(:cart_item).permit(:item_id, :amount, :image)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -21,21 +21,24 @@ Rails.application.routes.draw do
     get "customers/unsubscribe" => "customers#unsubscribe"
     patch "customers/withdraw" => "customers#withdraw"
     resources :addresses, only: [:index, :edit, :create, :update, :destroy]
-    resources :cart_items, only: [:index, :update, :destroy, :create]
     delete "cart_items/destroy_all" => "cart_items#destroy_all"
+    resources :cart_items, only: [:index, :update, :destroy, :create]
+    resources :orders, only: [:new, :create, :index, :show]
+    post "orders/confirm" => "orders#confirm"
+    get "orders/complete" => "orders#complete"
   end
 
   namespace :admin do
     resources :items, only: [:new, :show, :index, :create, :edit, :update]
-    
+
     resources :genres, only: [:edit, :update]
     get "genres" => "genres#index"
     post "genres" => "genres#create"
-    
+
     get "/" => "homes#top"
-    
+
     resources :customers, only: [:index, :show, :edit, :update]
-    
+
     resources :orders, only: [:show, :update]
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
