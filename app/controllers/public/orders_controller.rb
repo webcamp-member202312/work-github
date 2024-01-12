b801c7570fb40112858631b57c3cfe5572aec918
EC2 Default User <149689018+furumasa127@users.noreply.github.com>
2024-01-12 13:14:24 +0000

    [Finish]注文情報入力画面まで

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -3,21 +3,38 @@ class Public::OrdersController < ApplicationController
    
    def new
       @order = Order.new
-      @addresses = current_customer.addresses.all
    end
    
    def confirm
+      @order = Order.new(order_params)
+      @address = Address.find(params[:order][:address_id])
+      @order.postal_code = @address.postal_code
+      @order.address = @address.address
+      @order.name = @address.name = current_customer.first_name + current_customer.last_name
+      
+      @cart_items = current_customer.cart_items.all
    end
    
    def complete
    end
    
    def create
+      @order = Order.new(order_params)
+      @order.customer_id = current_customer.id
+      @selected_address = params[:order][:select_address]
+      @order.save
+      redirect_to orders_confirm_path
    end
    
    def index
    end
    
    def show
+   end
+   
+   private
+   
+   def order_params
+     params.require(:order).permit(:cutomer_id, :postal_code, :address, :name, :shipping_cost, :total_payment, :payment_method, :status)
    end
 end
